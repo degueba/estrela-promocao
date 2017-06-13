b8f90fde1afa764b32cb1fe2e6ec3d980c7f16c3
fabiano <fabiano.miranda@originalmedia.com.br>
2017-06-13 11:58:32 -0300

    Ajustes finos

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 16, 2017 22:00:00").getTime();
+var countDownDate = new Date("Aug 16, 2017 19:15:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
